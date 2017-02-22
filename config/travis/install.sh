c20f431c6139e7f5bf76d282e0521033bddd1167
ClaudiaSaxer <c.s.saxer@gmail.com>
2017-02-22 12:09:06 +0100

    last travis test

--- config/travis/install.sh
+++ config/travis/install.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-
     # Install some custom requirements on OS X
     # e.g. brew install pyenv-virtualenv
-
-  
 else
     # Install some custom requirements on Linux
 fi
