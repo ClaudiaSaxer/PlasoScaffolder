4d60ec35de1f94620c7ac1ff32125651e14b18f2
ClaudiaSaxer <c.s.saxer@gmail.com>
2017-02-22 12:13:45 +0100

    remove content for travis

--- config/travis/install.sh
+++ config/travis/install.sh
@@ -1,8 +1,1 @@
 #!/bin/bash
-
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    # Install some custom requirements on OS X
-    # e.g. brew install pyenv-virtualenv
-else
-    # Install some custom requirements on Linux
-fi
