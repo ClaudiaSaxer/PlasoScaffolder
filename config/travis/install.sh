dce4ec0d286c5254c1ba90055ea817a4040c98f4
ClaudiaSaxer <c.s.saxer@gmail.com>
2017-02-22 11:16:56 +0100

    matrix for travis

---
+++ config/travis/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+
+    # Install some custom requirements on OS X
+    # e.g. brew install pyenv-virtualenv
+
+  
+else
+    # Install some custom requirements on Linux
+fi
