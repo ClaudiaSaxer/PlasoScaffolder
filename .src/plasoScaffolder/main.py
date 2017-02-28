7634f2f9b200c3e95cac64414e01e1bcbf020f1f
Claudia <Claudia@DESKTOP-3NEFNNF>
2017-02-28 17:00:19 +0100

    simple argparse main

--- .src/plasoScaffolder/main.py
+++ .src/plasoScaffolder/main.py
@@ -3,11 +3,18 @@ import argparse
 
 def Main():
   parser = argparse.ArgumentParser()
-  parser.add_argument("-sqlite","--sqlite", help="SQLite Plugin",action="store_true")
+  group = parser.add_mutually_exclusive_group()
+  group.add_argument("-s","--sqlite", help="SQLite Plugin",action="store_true")
+  group.add_argument("-t","--test", help="Another Plugin",action="store_true")
+
+  parser.add_argument("-g","--guide",help="Plugin Creation Guide",action="store_true")
   args = parser.parse_args()
 
   if args.sqlite:
     print("sqlite")
+
+  if args.guide:
+    print("start the guide")
 
 if __name__ == '__main__':
   Main()
