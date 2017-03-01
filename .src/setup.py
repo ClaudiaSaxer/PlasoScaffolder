188803ee72e8d1bebc2bdbb3f1d1f6354e667072
Claudia <Claudia@DESKTOP-3NEFNNF>
2017-03-01 12:20:04 +0100

    delete file

--- .src/setup.py
+++ .src/setup.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-"""This is the setup file for the project. The standard setup rules apply:
-   python setup.py build
-   sudo python setup.py install
-"""
+"""This is the setup file for the project."""
 from setuptools import setup, find_packages
 
 setup(
