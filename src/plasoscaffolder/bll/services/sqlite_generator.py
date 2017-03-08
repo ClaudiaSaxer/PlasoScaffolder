f2d389f48db5f21df46224dfb3a57c67e44c498c
Claudia <Claudia@DESKTOP-3NEFNNF>
2017-03-08 14:48:21 +0100

    test generator

--- src/plasoscaffolder/bll/services/sqlite_generator.py
+++ src/plasoscaffolder/bll/services/sqlite_generator.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 """Module representing the sqlite plugin generator"""
-from plasoscaffolder.bll.services.file_handler import *
 from plasoscaffolder.bll.services.sqlite_plugin_helper import *
-import click
 
 
 class SqliteGenerator():
   """ Generator for SQLite Files """
 
-  def __init__(self, path, name, database):
+  def __init__(self, path, name, database,output):
     """ Constructs the SQLite Generator
 
     :param path: the path of the plaso folder
@@ -21,13 +19,14 @@ class SqliteGenerator():
     self.init_formatter_exists = file_exists(
       formatter_init_file_path(self.path))
     self.init_parser_exists = file_exists(parser_init_file_path(self.path))
+    self.output = output
 
-  def generate_sqlite_plugin(self):
-    creator = FileHandler()
+  def generate_sqlite_plugin(self, fileHandler):
+    file_handler = fileHandler()
 
-    file = creator.create_file_from_path
-    copy = creator.copy_file
-    edit = creator.add_content
+    file = file_handler.create_file_from_path
+    copy = file_handler.copy_file
+    edit = file_handler.add_content
 
     content_init_formatter = "from plaso.formatters import " + self.name
     content_init_parser = "from plaso.parsers.sqlite_plugins import " + \
@@ -44,45 +43,42 @@ class SqliteGenerator():
       content_init_formatter,
       self.init_formatter_exists)
 
-    self.__print(formatter, parser, formatter_test, parser_test, database,
+    self._print(formatter, parser, formatter_test, parser_test, database,
       parser_init, formatter_init)
 
-  def __print(self,formatter, parser, formatter_test, parser_test, database,
+  def _print(self,formatter, parser, formatter_test, parser_test, database,
       parser_init, formatter_init):
-    self.__print_create(formatter)
-    self.__print_create(parser)
-    self.__print_create(formatter_test)
-    self.__print_create(parser_test)
-    self.__print_copy(database)
+    self._print_create(formatter)
+    self._print_create(parser)
+    self._print_create(formatter_test)
+    self._print_create(parser_test)
+    self._print_copy(database)
     if self.init_parser_exists:
-      self.__print_edit(parser_init)
+      self._print_edit(parser_init)
     else:
-      self.__print_create(parser_init)
+      self._print_create(parser_init)
     if self.init_formatter_exists:
-      self.__print_edit(formatter_init)
+      self._print_edit(formatter_init)
     else:
-      self.__print_create(formatter_init)
+      self._print_create(formatter_init)
 
-  @staticmethod
-  def __print_copy(file):
+  def _print_copy(self,file):
     """
     Click echo for copy file
     :param file: the file path
     """
-    click.echo("copy " + file)
+    self.output("copy " + file)
 
-  @staticmethod
-  def __print_edit(file):
+  def _print_edit(self,file):
     """
     Click echo for edit file
     :param file: the file path
     """
-    click.echo("edit " + file)
+    self.output("edit " + file)
 
-  @staticmethod
-  def __print_create(file):
+  def _print_create(self,file):
     """
     Click echo for create file
     :param file: the file path
     """
-    click.echo("create " + file)
+    self.output("create " + file)
