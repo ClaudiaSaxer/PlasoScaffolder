4614be57d5700ca35d2ec1c4955227438478bda1
Claudia <Claudia@DESKTOP-3NEFNNF>
2017-03-08 16:12:48 +0100

    format

--- src/tests/services/test_init_formatter_mapping.py
+++ src/tests/services/test_init_formatter_mapping.py
@@ -4,33 +4,37 @@ from plasoscaffolder.bll.mappings.init_mapping import *
 
 
 class InitMappingTest(unittest.TestCase):
-""" Class representing tests for the init mapping"""
+  """ Class representing tests for the init mapping"""
+
   def setUp(self):
     self.plugin_name = "the_one_and_only"
 
   def test_get_formatter_init_create(self):
     """test the render for creating a new formatter init file"""
     actual = get_formatter_init_create(self.plugin_name)
-    expected = "# -*- coding: utf-8 -*-\nfrom plaso.formatters import "+self.plugin_name
-    self.assertEqual(expected,actual)
+    expected = "# -*- coding: utf-8 -*-\nfrom plaso.formatters import " + \
+               self.plugin_name
+    self.assertEqual(expected, actual)
 
   def test_get_formatter_init_edit(self):
     """test the render for editing a existing formatter init file"""
     actual = get_formatter_init_edit(self.plugin_name)
-    expected = "from plaso.formatters import "+self.plugin_name
-    self.assertEqual(expected,actual)
+    expected = "from plaso.formatters import " + self.plugin_name
+    self.assertEqual(expected, actual)
 
   def test_get_parser_init_create(self):
     """test the render for creating a new parser init file"""
     actual = get_parser_init_create(self.plugin_name)
-    expected = "# -*- coding: utf-8 -*-\nfrom plaso.parsers.sqlite_plugins import "+self.plugin_name
-    self.assertEqual(expected,actual)
+    expected = "# -*- coding: utf-8 -*-\nfrom plaso.parsers.sqlite_plugins " \
+               "import " + self.plugin_name
+    self.assertEqual(expected, actual)
 
   def test_get_parser_init_edit(self):
     """test the render for editing a existing parser init file"""
     actual = get_parser_init_edit(self.plugin_name)
-    expected = "from plaso.parsers.sqlite_plugins import "+self.plugin_name
-    self.assertEqual(expected,actual)
+    expected = "from plaso.parsers.sqlite_plugins import " + self.plugin_name
+    self.assertEqual(expected, actual)
+
 
 if __name__ == '__main__':
   unittest.main()
