b4bd22355c61e38c5dc1b7cb2a3d34b7977ef920
Claudia <Claudia@DESKTOP-3NEFNNF>
2017-03-08 10:36:15 +0100

    remove travis config and just pip requirements

--- config/travis/installnotworking.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    # Install some custom requirements on OS X
-    # e.g. brew install pyenv-virtualenv
-else
-    # Install some custom requirements on Linux
-fi
