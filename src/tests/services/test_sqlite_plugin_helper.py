9a02db98f4e4e604e6f4fe96e801beadc1a39162
Claudia <Claudia@DESKTOP-3NEFNNF>
2017-03-13 18:27:55 +0100

    format

--- src/tests/services/test_sqlite_plugin_helper.py
+++ src/tests/services/test_sqlite_plugin_helper.py
@@ -12,21 +12,23 @@ class SqlitePluginHelperTest(unittest.TestCase):
   def setUp(self):
     path = "temp"
     plugin_name = "plugin_test"
-    self.fake_path_helper = FakeSqlitePluginPathHelper(path,plugin_name)
+    self.fake_path_helper = FakeSqlitePluginPathHelper(path, plugin_name)
     self.helper = SqlitePluginHelper()
 
   def test_plugin_exists_false(self):
     """Tests the plugin exists method if none exists."""
 
-    actual = self.helper.plugin_exists('temp','plugin_test',self.fake_path_helper)
+    actual = self.helper.plugin_exists('temp', 'plugin_test',
+      self.fake_path_helper)
     self.assertFalse(actual)
 
   def test_plugin_exists_true(self):
-    """Tests the plugin exists method if the parser file exists."""
+    """Tests the plugin exists"""
     with tempfile.TemporaryDirectory() as tmpdir:
       with tempfile.TemporaryFile(dir=tmpdir) as fp:
         helper = SqlitePluginHelper()
-        actual = helper.plugin_exists(tmpdir, fp.name, FakeSqlitePluginPathHelper(tmpdir,fp.name))
+        actual = helper.plugin_exists(tmpdir, fp.name,
+          FakeSqlitePluginPathHelper(tmpdir, fp.name))
 
     self.assertTrue(actual)
 
@@ -47,8 +49,6 @@ class SqlitePluginHelperTest(unittest.TestCase):
       actual = helper.folder_exists(tmpdir)
 
     self.assertTrue(actual)
-
-
 
 
 if __name__ == '__main__':
