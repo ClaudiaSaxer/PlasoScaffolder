f5872d8641d9341545dd269c7dd86ab7ba85801b
Claudia Saxer <c.s.saxer@gmail.com>
2017-03-14 13:27:31 +0100

    test frontend sqlite --help

--- src/tests/frontend/test_main.py
+++ src/tests/frontend/test_main.py
@@ -1,25 +1,19 @@
 import unittest
-import click
 from click.testing import CliRunner
 from plasoscaffolder.frontend.main import entry_point
 
 
-class MyTestCase(unittest.TestCase):
-  def test_sqlite_help(self):
+class MainTest(unittest.TestCase):
+  def test_main_help(self):
     runner = CliRunner()
     result = runner.invoke(entry_point, ['--help'])
     expected_output = (
-      'Usage: entry_point [OPTIONS] COMMAND [ARGS]...'
+      'Usage: entry_point [OPTIONS] COMMAND [ARGS]...\n'
       '\n'
+      'Options:\n'
+      '  --help  Show this message and exit.\n'
       '\n'
-      'Options:'
-      '\n'
-      '  --help  '
-      'Show this message and exit.'
-      '\n'
-      '\n'
-      'Commands:'
-      '\n'
+      'Commands:''\n'
       '  sqlite\n'
     )
 
