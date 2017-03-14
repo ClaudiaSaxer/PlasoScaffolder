95cc3a08b4be359d21f8ba6afec5cd2f646b3354
Claudia Saxer <c.s.saxer@gmail.com>
2017-03-14 13:56:08 +0100

    add test

--- src/tests/frontend/sqliteplugin/test_commands.py
+++ src/tests/frontend/sqliteplugin/test_commands.py
@@ -1,24 +1,24 @@
 import unittest
 
 from click.testing import CliRunner
-from plasoscaffolder.frontend.main import entry_point
+from plasoscaffolder.frontend.sqliteplugin.commands import sqlite
 
 
 class SqliteCommandsTest(unittest.TestCase):
-
   def test_sqlite_help(self):
     runner = CliRunner()
-    result = runner.invoke(entry_point, ['sqlite', '--help'])
+    result = runner.invoke(sqlite, ['--help'])
     expected_output = (
-    'Usage: entry_point sqlite [OPTIONS]\n'
-    '\n'
-    'Options:\n'
-    '  -p, --path TEXT  The path to plaso\n'
-    '  --name TEXT      The plugin name\n'
-    '  --testfile TEXT  The testfile path\n'
-    '  --help           Show this message and exit.\n')
+      'Usage: sqlite [OPTIONS]\n'
+      '\n'
+      'Options:\n'
+      '  -p, --path TEXT  The path to plaso\n'
+      '  --name TEXT      The plugin name\n'
+      '  --testfile TEXT  The testfile path\n'
+      '  --help           Show this message and exit.\n')
     self.assertEqual(expected_output, str(result.output))
     self.assertEqual(0, result.exit_code)
+
 
 if __name__ == '__main__':
   unittest.main()
--- src/tests/frontend/test_main.py
+++ src/tests/frontend/test_main.py
@@ -20,6 +20,19 @@ class MainTest(unittest.TestCase):
     self.assertEqual(expected_output, str(result.output))
     self.assertEqual(0, result.exit_code)
 
+  def test_sqlite_help(self):
+    runner = CliRunner()
+    result = runner.invoke(entry_point, ['sqlite', '--help'])
+    expected_output = (
+    'Usage: entry_point sqlite [OPTIONS]\n'
+    '\n'
+    'Options:\n'
+    '  -p, --path TEXT  The path to plaso\n'
+    '  --name TEXT      The plugin name\n'
+    '  --testfile TEXT  The testfile path\n'
+    '  --help           Show this message and exit.\n')
+    self.assertEqual(expected_output, str(result.output))
+    self.assertEqual(0, result.exit_code)
 
 if __name__ == '__main__':
   unittest.main()
