bb2c98543d57ef321b3ec31418f9293328d93368
Claudia Saxer <c.s.saxer@gmail.com>
2017-03-14 12:02:19 +0100

    first frontend test

--- src/tests/frontend/test_main.py
+++ src/tests/frontend/test_main.py
@@ -5,14 +5,27 @@ from plasoscaffolder.frontend.main import entry_point
 
 
 class MyTestCase(unittest.TestCase):
-  def test_something(self):
-    self.assertEqual(True, False)
-
-  def test_hello_world(self):
+  def test_sqlite_help(self):
     runner = CliRunner()
     result = runner.invoke(entry_point, ['--help'])
-    assert result.exit_code == 0
-    assert 'Debug mode is on' in result.output
-    assert 'Syncing' in result.output
+    expected_output = (
+      "Usage: entry_point [OPTIONS] COMMAND [ARGS]..."
+      "\n"
+      "\n"
+      "Options:"
+      "\n"
+      "  --help  "
+      "Show this message and exit."
+      "\n"
+      "\n"
+      "Commands:"
+      "\n"
+      "  sqlite\n"
+    )
+
+    self.assertEqual(expected_output, str(result.output))
+    self.assertEqual(0, result.exit_code)
+
+
 if __name__ == '__main__':
   unittest.main()
