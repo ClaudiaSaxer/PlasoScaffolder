815284f3a1d954525c6b6f2ed250cc282a3400a1
Claudia Saxer <c.s.saxer@gmail.com>
2017-04-10 11:11:12 +0200

    change type description of queryexecution

--- src/plasoscaffolder/bll/services/base_sqlite_plugin_helper.py
+++ src/plasoscaffolder/bll/services/base_sqlite_plugin_helper.py
@@ -24,7 +24,7 @@ class BaseSQLitePluginHelper(object):
       database_suffix: the suffix of the database file
       path (str): the path of the plaso source
       plugin_name (str): the name of the plugin
-      path_helper (BaseSqlitePluginHelper) : the sqlite plugin helper
+      path_helper (BaseSqlitePluginHelper): the sqlite plugin helper
 
     Returns:
       bool: True if the plugin already exists. False if it does not.
@@ -59,11 +59,12 @@ class BaseSQLitePluginHelper(object):
 
   @abc.abstractmethod
   def RunSQLQuery(self, query: str,
-                  executor: base_sql_query_execution.BaseSQLQueryExecution()):
+                  executor: base_sql_query_execution.BaseSQLQueryExecution):
     """ Validates the sql query
 
     Args:
-      executor (base_sql_query_execution.SQLQueryExection()) the sql executor
+      executor (base_sql_query_execution.SQLQueryExecution): to validate the 
+      SQL queries provided by the user
       query (str): the sql query 
 
     Returns:
