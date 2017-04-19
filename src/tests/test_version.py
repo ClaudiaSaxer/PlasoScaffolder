944e4f68392ac008371bf0bc50f59ceb5f7af2ba
Claudia Saxer <c.s.saxer@gmail.com>
2017-04-19 13:11:34 +0200

    another version test for not dev

--- src/tests/test_version.py
+++ src/tests/test_version.py
@@ -8,13 +8,19 @@ import plasoscaffolder
 class VersionTest(unittest.TestCase):
   """the version in init test"""
 
-  def testGetVersion(self):
+  def testGetVersionDev(self):
     """testing the get version"""
     expected = '{0}_{1}'.format(
         plasoscaffolder.__version__, plasoscaffolder.VERSION_DATE)
     actual = plasoscaffolder.GetVersion()
     self.assertEqual(expected, actual)
 
+  def testGetVersionNotDev(self):
+    """testing the get version"""
+    expected = plasoscaffolder.__version__
+    plasoscaffolder.VERSION_DEV = False
+    actual = plasoscaffolder.GetVersion()
+    self.assertEqual(expected, actual)
 
 if __name__ == '__main__':
   unittest.main()
