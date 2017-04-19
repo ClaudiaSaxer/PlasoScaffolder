5418a63530aa2696df511c853bd4d11171b20746
Claudia Saxer <c.s.saxer@gmail.com>
2017-04-19 11:40:34 +0200

    test dependencies

--- src/tests/test_dependencies.py
+++ src/tests/test_dependencies.py
@@ -2,12 +2,25 @@
 """testing the dependencies"""
 import unittest
 
+from plasoscaffolder import dependencies
+
 
 class DependenciesTest(unittest.TestCase):
   """the dependencies test"""
 
-  def testVersion(self):
-    """testing the """
+  def testDependencies(self):
+    """testing that for each python dependencies there is a """
+    python_keys = dependencies.PYTHON_DEPENDENCIES.keys()
+    dpkg_keys = dependencies._DPKG_PACKAGE_NAMES.keys()
+    pypi_keys = dependencies._PYPI_PROJECT_NAMES.keys()
+    rpm_keys = dependencies._RPM_PACKAGE_NAMES.keys()
+    python_test_keys = dependencies.PYTHON_TEST_DEPENDENCIES
+
+    self.assertNotEqual(python_keys, [])
+    self.assertEqual(python_keys, dpkg_keys)
+    self.assertEqual(python_keys, pypi_keys)
+    self.assertEqual(pypi_keys, rpm_keys)
+    self.assertIsNotNone(python_test_keys)
 
 
 if __name__ == '__main__':
