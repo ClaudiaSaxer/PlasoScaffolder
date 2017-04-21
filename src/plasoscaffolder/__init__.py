87a8ba545eeaeffd75fa107078418b54bd108590
Claudia Saxer <c.s.saxer@gmail.com>
2017-04-21 12:03:35 +0200

    change version to date

--- src/plasoscaffolder/__init__.py
+++ src/plasoscaffolder/__init__.py
@@ -1,17 +1,2 @@
 # -*- coding: utf-8 -*-
-__version__ = '1.0.0'
-
-VERSION_DEV = True
-VERSION_DATE = '20170413'
-
-
-def GetVersion():
-  """Retrieves the version.
-
-  Returns:
-    A string containing the version.
-  """
-  if VERSION_DEV:
-    return u'{0:s}_{1:s}'.format(__version__, VERSION_DATE)
-
-  return __version__
+__version__ = '20170614'
