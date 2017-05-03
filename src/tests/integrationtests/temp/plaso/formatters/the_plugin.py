fb91215955aaed9977b7b4d01e357bebd7705c1e
Claudia Saxer <c.s.saxer@gmail.com>
2017-05-03 15:10:33 +0200

    add timestamp to parser test template

--- src/tests/integrationtests/temp/plaso/formatters/the_plugin.py
+++ src/tests/integrationtests/temp/plaso/formatters/the_plugin.py
@@ -6,51 +6,19 @@ from plaso.formatters import manager
 from plaso.lib import errors
 
 
-class ThePluginUsersFormatter(interface.ConditionalEventFormatter):
-  """the plugin users event formatter."""
+class ThePluginBlobtypesFormatter(interface.ConditionalEventFormatter):
+  """the plugin blobtypes event formatter."""
 
-  DATA_TYPE = u'the:plugin:users'
+  DATA_TYPE = u'the:plugin:blobtypes'
 
   """Correct Format String Pieces where needed"""
   FORMAT_STRING_PIECES = [
-    u'Advertiser Account Type:{advertiser_account_type}',
-    u'Analytics Type:{analytics_type}',
-    u'Bio Entities:{bio_entities}',
-    u'Business Profile State:{business_profile_state}',
-    u'Could Be Stale:{could_be_stale}',
-    u'Description:{description}',
-    u'Device Following:{device_following}',
-    u'Extended Profile Fields:{extended_profile_fields}',
-    u'Favorites Count:{favorites_count}',
-    u'Followers Count:{followers_count}',
-    u'Followers Count Fast:{followers_count_fast}',
-    u'Followers Count Normal:{followers_count_normal}',
-    u'Following:{following}',
-    u'Following Count:{following_count}',
-    u'Has Collections:{has_collections}',
-    u'Has Extended Profile Fields:{has_extended_profile_fields}',
-    u'Id:{id}',
-    u'Is Lifeline Institution:{is_lifeline_institution}',
-    u'Is Translator:{is_translator}',
-    u'Location:{location}',
-    u'Media Count:{media_count}',
-    u'Name:{name}',
-    u'Pinned Tweet Id:{pinned_tweet_id}',
-    u'Profile Banner Url:{profile_banner_url}',
-    u'Profile Image Url:{profile_image_url}',
-    u'Profile Link Color Hex Triplet:{profile_link_color_hex_triplet}',
-    u'Protected:{protected}',
-    u'Screen Name:{screen_name}',
-    u'Statuses Count:{statuses_count}',
-    u'Structured Location:{structured_location}',
-    u'Url:{url}',
-    u'Url Entities:{url_entities}',
-    u'Verified:{verified}']
+    u'Blobval:{blobval}']
 
   #TODO: add Format String Pieces for the short Format
   FORMAT_STRING_SHORT_PIECES = []
 
-  SOURCE_LONG = u'The Plugin Users'
+  SOURCE_LONG = u'The Plugin Blobtypes'
   SOURCE_SHORT = u'The Plugin'
   #TODO: Replace constant _REPLACEWITHATTRIBUTENAME with the attribute to customize and add values
   _REPLACEWITHATTRIBUTENAME = {}
@@ -86,51 +54,297 @@ class ThePluginUsersFormatter(interface.ConditionalEventFormatter):
 
 
 
-class ThePluginStatusesFormatter(interface.ConditionalEventFormatter):
-  """the plugin statuses event formatter."""
+class ThePluginIntegertypesFormatter(interface.ConditionalEventFormatter):
+  """the plugin integertypes event formatter."""
 
-  DATA_TYPE = u'the:plugin:statuses'
+  DATA_TYPE = u'the:plugin:integertypes'
 
   """Correct Format String Pieces where needed"""
   FORMAT_STRING_PIECES = [
-    u'Card:{card}',
-    u'Card Users:{card_users}',
-    u'Card Version:{card_version}',
-    u'Entities:{entities}',
-    u'Extra Scribe Item:{extra_scribe_item}',
-    u'Favorite Count:{favorite_count}',
-    u'Favorited:{favorited}',
-    u'Full Text Length:{full_text_length}',
-    u'Geotag:{geotag}',
-    u'Id:{id}',
-    u'Include In Profile Timeline:{include_in_profile_timeline}',
-    u'In Reply To Status Id:{in_reply_to_status_id}',
-    u'In Reply To Username:{in_reply_to_username}',
-    u'Is Lifeline Alert:{is_lifeline_alert}',
-    u'Is Possibly Sensitive Appealable:{is_possibly_sensitive_appealable}',
-    u'Is Truncated:{is_truncated}',
-    u'Lang:{lang}',
-    u'Possibly Sensitive:{possibly_sensitive}',
-    u'Preview Length:{preview_length}',
-    u'Primary Card Type:{primary_card_type}',
-    u'Quoted Status Id:{quoted_status_id}',
-    u'Retweet Count:{retweet_count}',
-    u'Retweeted Status Id:{retweeted_status_id}',
-    u'Source:{source}',
-    u'Supplmental Language:{supplmental_language}',
-    u'Text:{text}',
-    u'User Id:{user_id}',
-    u'Withheld In Countries:{withheld_in_countries}',
-    u'Withheld Scope:{withheld_scope}']
+    u'Bigintval:{bigintval}',
+    u'Int2Val:{int2val}',
+    u'Int8Val:{int8val}',
+    u'Integerval:{integerval}',
+    u'Intval:{intval}',
+    u'Mediumintval:{mediumintval}',
+    u'Smallintval:{smallintval}',
+    u'Tinyintval:{tinyintval}',
+    u'Unsignedbigintval:{unsignedbigintval}']
 
   #TODO: add Format String Pieces for the short Format
   FORMAT_STRING_SHORT_PIECES = []
 
-  SOURCE_LONG = u'The Plugin Statuses'
+  SOURCE_LONG = u'The Plugin Integertypes'
   SOURCE_SHORT = u'The Plugin'
-  
+  #TODO: Replace constant _REPLACEWITHATTRIBUTENAME with the attribute to customize and add values
+  _REPLACEWITHATTRIBUTENAME = {}
+
+  def GetMessages(self, unused_formatter_mediator, event):
+    """Determines the formatted message strings for an event object.
+
+    Args:
+      formatter_mediator (FormatterMediator): mediates the interactions between
+          formatters and other components, such as storage and Windows EventLog
+          resources.
+      event (EventObject): event.
+
+    Returns:
+      tuple(str, str): formatted message string and short message string.
+
+    Raises:
+      WrongFormatter: if the event object cannot be formatted by the formatter.
+    """
+    if self.DATA_TYPE != event.data_type:
+      raise errors.WrongFormatter(u'Unsupported data type: {0:s}.'.format(
+          event.data_type))
+
+    event_values = event.CopyToDict()
+
+    # TODO: replace variable replace_with_attribute_name with the attribute to customize
+    replace_with_attribute_name = event_values.get(u'replace_with_attribute_name', None)
+    if replace_with_attribute_name is not None:
+      event_values[u'replace_with_attribute_name'] = (
+          self._REPLACEWITHATTRIBUTENAME.get(replace_with_attribute_name, u'UNKNOWN'))
+
+    return self._ConditionalFormatMessages(event_values)
+
+
+
+class ThePluginNumerictypesFormatter(interface.ConditionalEventFormatter):
+  """the plugin numerictypes event formatter."""
+
+  DATA_TYPE = u'the:plugin:numerictypes'
+
+  """Correct Format String Pieces where needed"""
+  FORMAT_STRING_PIECES = [
+    u'Booleanval:{booleanval}',
+    u'Datetimeval:{datetimeval}',
+    u'Dateval:{dateval}',
+    u'Decimalval:{decimalval}',
+    u'Numericval:{numericval}']
+
+  #TODO: add Format String Pieces for the short Format
+  FORMAT_STRING_SHORT_PIECES = []
+
+  SOURCE_LONG = u'The Plugin Numerictypes'
+  SOURCE_SHORT = u'The Plugin'
+  #TODO: Replace constant _REPLACEWITHATTRIBUTENAME with the attribute to customize and add values
+  _REPLACEWITHATTRIBUTENAME = {}
+
+  def GetMessages(self, unused_formatter_mediator, event):
+    """Determines the formatted message strings for an event object.
+
+    Args:
+      formatter_mediator (FormatterMediator): mediates the interactions between
+          formatters and other components, such as storage and Windows EventLog
+          resources.
+      event (EventObject): event.
+
+    Returns:
+      tuple(str, str): formatted message string and short message string.
+
+    Raises:
+      WrongFormatter: if the event object cannot be formatted by the formatter.
+    """
+    if self.DATA_TYPE != event.data_type:
+      raise errors.WrongFormatter(u'Unsupported data type: {0:s}.'.format(
+          event.data_type))
+
+    event_values = event.CopyToDict()
+
+    # TODO: replace variable replace_with_attribute_name with the attribute to customize
+    replace_with_attribute_name = event_values.get(u'replace_with_attribute_name', None)
+    if replace_with_attribute_name is not None:
+      event_values[u'replace_with_attribute_name'] = (
+          self._REPLACEWITHATTRIBUTENAME.get(replace_with_attribute_name, u'UNKNOWN'))
+
+    return self._ConditionalFormatMessages(event_values)
+
+
+
+class ThePluginRealtypesFormatter(interface.ConditionalEventFormatter):
+  """the plugin realtypes event formatter."""
+
+  DATA_TYPE = u'the:plugin:realtypes'
+
+  """Correct Format String Pieces where needed"""
+  FORMAT_STRING_PIECES = [
+    u'Doubleprecesionval:{doubleprecesionval}',
+    u'Doubleval:{doubleval}',
+    u'Floatval:{floatval}',
+    u'Realval:{realval}']
+
+  #TODO: add Format String Pieces for the short Format
+  FORMAT_STRING_SHORT_PIECES = []
+
+  SOURCE_LONG = u'The Plugin Realtypes'
+  SOURCE_SHORT = u'The Plugin'
+  #TODO: Replace constant _REPLACEWITHATTRIBUTENAME with the attribute to customize and add values
+  _REPLACEWITHATTRIBUTENAME = {}
+
+  def GetMessages(self, unused_formatter_mediator, event):
+    """Determines the formatted message strings for an event object.
+
+    Args:
+      formatter_mediator (FormatterMediator): mediates the interactions between
+          formatters and other components, such as storage and Windows EventLog
+          resources.
+      event (EventObject): event.
+
+    Returns:
+      tuple(str, str): formatted message string and short message string.
+
+    Raises:
+      WrongFormatter: if the event object cannot be formatted by the formatter.
+    """
+    if self.DATA_TYPE != event.data_type:
+      raise errors.WrongFormatter(u'Unsupported data type: {0:s}.'.format(
+          event.data_type))
+
+    event_values = event.CopyToDict()
+
+    # TODO: replace variable replace_with_attribute_name with the attribute to customize
+    replace_with_attribute_name = event_values.get(u'replace_with_attribute_name', None)
+    if replace_with_attribute_name is not None:
+      event_values[u'replace_with_attribute_name'] = (
+          self._REPLACEWITHATTRIBUTENAME.get(replace_with_attribute_name, u'UNKNOWN'))
+
+    return self._ConditionalFormatMessages(event_values)
+
+
+
+class ThePluginTexttypesFormatter(interface.ConditionalEventFormatter):
+  """the plugin texttypes event formatter."""
+
+  DATA_TYPE = u'the:plugin:texttypes'
+
+  """Correct Format String Pieces where needed"""
+  FORMAT_STRING_PIECES = [
+    u'Characterval:{characterval}',
+    u'Clobval:{clobval}',
+    u'Nativecharacterval:{nativecharacterval}',
+    u'Ncharval:{ncharval}',
+    u'Nvarchar Val:{nvarchar_val}',
+    u'Textval:{textval}',
+    u'Varcharval:{varcharval}',
+    u'Varyingcharacterval:{varyingcharacterval}']
+
+  #TODO: add Format String Pieces for the short Format
+  FORMAT_STRING_SHORT_PIECES = []
+
+  SOURCE_LONG = u'The Plugin Texttypes'
+  SOURCE_SHORT = u'The Plugin'
+  #TODO: Replace constant _REPLACEWITHATTRIBUTENAME with the attribute to customize and add values
+  _REPLACEWITHATTRIBUTENAME = {}
+
+  def GetMessages(self, unused_formatter_mediator, event):
+    """Determines the formatted message strings for an event object.
+
+    Args:
+      formatter_mediator (FormatterMediator): mediates the interactions between
+          formatters and other components, such as storage and Windows EventLog
+          resources.
+      event (EventObject): event.
+
+    Returns:
+      tuple(str, str): formatted message string and short message string.
+
+    Raises:
+      WrongFormatter: if the event object cannot be formatted by the formatter.
+    """
+    if self.DATA_TYPE != event.data_type:
+      raise errors.WrongFormatter(u'Unsupported data type: {0:s}.'.format(
+          event.data_type))
+
+    event_values = event.CopyToDict()
+
+    # TODO: replace variable replace_with_attribute_name with the attribute to customize
+    replace_with_attribute_name = event_values.get(u'replace_with_attribute_name', None)
+    if replace_with_attribute_name is not None:
+      event_values[u'replace_with_attribute_name'] = (
+          self._REPLACEWITHATTRIBUTENAME.get(replace_with_attribute_name, u'UNKNOWN'))
+
+    return self._ConditionalFormatMessages(event_values)
+
+
+
+class ThePluginNodataFormatter(interface.ConditionalEventFormatter):
+  """the plugin nodata event formatter."""
+
+  DATA_TYPE = u'the:plugin:nodata'
+
+  """Correct Format String Pieces where needed"""
+  FORMAT_STRING_PIECES = [
+    u'Bigintval:{bigintval}',
+    u'Blobval:{blobval}',
+    u'Booleanval:{booleanval}',
+    u'Characterval:{characterval}',
+    u'Clobval:{clobval}',
+    u'Datetimeval:{datetimeval}',
+    u'Dateval:{dateval}',
+    u'Decimalval:{decimalval}',
+    u'Doubleprecisionval:{doubleprecisionval}',
+    u'Doubleval:{doubleval}',
+    u'Floatval:{floatval}',
+    u'Int2Val:{int2val}',
+    u'Int8Val:{int8val}',
+    u'Integerval:{integerval}',
+    u'Intval:{intval}',
+    u'Mediuintval:{mediuintval}',
+    u'Nativecharacterval:{nativecharacterval}',
+    u'Ncharval:{ncharval}',
+    u'Numericval:{numericval}',
+    u'Nvarcharval:{nvarcharval}',
+    u'Realval:{realval}',
+    u'Smallintval:{smallintval}',
+    u'Textval:{textval}',
+    u'Tinyintval:{tinyintval}',
+    u'Unsignedbigintval:{unsignedbigintval}',
+    u'Varcharval:{varcharval}',
+    u'Varyingcharacterval:{varyingcharacterval}']
+
+  #TODO: add Format String Pieces for the short Format
+  FORMAT_STRING_SHORT_PIECES = []
+
+  SOURCE_LONG = u'The Plugin Nodata'
+  SOURCE_SHORT = u'The Plugin'
+  #TODO: Replace constant _REPLACEWITHATTRIBUTENAME with the attribute to customize and add values
+  _REPLACEWITHATTRIBUTENAME = {}
+
+  def GetMessages(self, unused_formatter_mediator, event):
+    """Determines the formatted message strings for an event object.
+
+    Args:
+      formatter_mediator (FormatterMediator): mediates the interactions between
+          formatters and other components, such as storage and Windows EventLog
+          resources.
+      event (EventObject): event.
+
+    Returns:
+      tuple(str, str): formatted message string and short message string.
+
+    Raises:
+      WrongFormatter: if the event object cannot be formatted by the formatter.
+    """
+    if self.DATA_TYPE != event.data_type:
+      raise errors.WrongFormatter(u'Unsupported data type: {0:s}.'.format(
+          event.data_type))
+
+    event_values = event.CopyToDict()
+
+    # TODO: replace variable replace_with_attribute_name with the attribute to customize
+    replace_with_attribute_name = event_values.get(u'replace_with_attribute_name', None)
+    if replace_with_attribute_name is not None:
+      event_values[u'replace_with_attribute_name'] = (
+          self._REPLACEWITHATTRIBUTENAME.get(replace_with_attribute_name, u'UNKNOWN'))
+
+    return self._ConditionalFormatMessages(event_values)
 
 manager.FormattersManager.RegisterFormatter([
-  ThePluginUsersFormatter,
-  ThePluginStatusesFormatter
+  ThePluginBlobtypesFormatter,
+  ThePluginIntegertypesFormatter,
+  ThePluginNumerictypesFormatter,
+  ThePluginRealtypesFormatter,
+  ThePluginTexttypesFormatter,
+  ThePluginNodataFormatter
 ])
--- src/tests/integrationtests/temp/tests/formatters/the_plugin.py
+++ src/tests/integrationtests/temp/tests/formatters/the_plugin.py
@@ -7,99 +7,163 @@ import unittest
 from plaso.formatters import the_plugin
 from tests.formatters import test_lib
 
-class ThePluginUsersFormatterTest(test_lib.EventFormatterTestCase):
-  """Tests the The Plugin users event formatter."""
+class ThePluginBlobtypesFormatterTest(test_lib.EventFormatterTestCase):
+  """Tests the The Plugin blobtypes event formatter."""
 
   def testInitialization(self):
     """Tests the initialization."""
-    event_formatter = the_plugin.ThePluginUsersFormatter()
+    event_formatter = the_plugin.ThePluginBlobtypesFormatter()
     self.assertIsNotNone(event_formatter)
 
   def testGetFormatStringAttributeNames(self):
     """Tests the GetFormatStringAttributeNames function."""
-    event_formatter = the_plugin.ThePluginUsersFormatter()
+    event_formatter = the_plugin.ThePluginBlobtypesFormatter()
 
     expected_attribute_names = [
-        u'advertiser_account_type',
-        u'analytics_type',
-        u'bio_entities',
-        u'business_profile_state',
-        u'could_be_stale',
-        u'description',
-        u'device_following',
-        u'extended_profile_fields',
-        u'favorites_count',
-        u'followers_count',
-        u'followers_count_fast',
-        u'followers_count_normal',
-        u'following',
-        u'following_count',
-        u'has_collections',
-        u'has_extended_profile_fields',
-        u'id',
-        u'is_lifeline_institution',
-        u'is_translator',
-        u'location',
-        u'media_count',
-        u'name',
-        u'pinned_tweet_id',
-        u'profile_banner_url',
-        u'profile_image_url',
-        u'profile_link_color_hex_triplet',
-        u'protected',
-        u'screen_name',
-        u'statuses_count',
-        u'structured_location',
-        u'url',
-        u'url_entities',
-        u'verified'
+        u'blobval'
         ]
 
     self._TestGetFormatStringAttributeNames(
         event_formatter, expected_attribute_names)
 
-class ThePluginStatusesFormatterTest(test_lib.EventFormatterTestCase):
-  """Tests the The Plugin statuses event formatter."""
+class ThePluginIntegertypesFormatterTest(test_lib.EventFormatterTestCase):
+  """Tests the The Plugin integertypes event formatter."""
 
   def testInitialization(self):
     """Tests the initialization."""
-    event_formatter = the_plugin.ThePluginStatusesFormatter()
+    event_formatter = the_plugin.ThePluginIntegertypesFormatter()
     self.assertIsNotNone(event_formatter)
 
   def testGetFormatStringAttributeNames(self):
     """Tests the GetFormatStringAttributeNames function."""
-    event_formatter = the_plugin.ThePluginStatusesFormatter()
+    event_formatter = the_plugin.ThePluginIntegertypesFormatter()
 
     expected_attribute_names = [
-        u'card',
-        u'card_users',
-        u'card_version',
-        u'entities',
-        u'extra_scribe_item',
-        u'favorite_count',
-        u'favorited',
-        u'full_text_length',
-        u'geotag',
-        u'id',
-        u'include_in_profile_timeline',
-        u'in_reply_to_status_id',
-        u'in_reply_to_username',
-        u'is_lifeline_alert',
-        u'is_possibly_sensitive_appealable',
-        u'is_truncated',
-        u'lang',
-        u'possibly_sensitive',
-        u'preview_length',
-        u'primary_card_type',
-        u'quoted_status_id',
-        u'retweet_count',
-        u'retweeted_status_id',
-        u'source',
-        u'supplmental_language',
-        u'text',
-        u'user_id',
-        u'withheld_in_countries',
-        u'withheld_scope'
+        u'bigintval',
+        u'int2val',
+        u'int8val',
+        u'integerval',
+        u'intval',
+        u'mediumintval',
+        u'smallintval',
+        u'tinyintval',
+        u'unsignedbigintval'
+        ]
+
+    self._TestGetFormatStringAttributeNames(
+        event_formatter, expected_attribute_names)
+
+class ThePluginNumerictypesFormatterTest(test_lib.EventFormatterTestCase):
+  """Tests the The Plugin numerictypes event formatter."""
+
+  def testInitialization(self):
+    """Tests the initialization."""
+    event_formatter = the_plugin.ThePluginNumerictypesFormatter()
+    self.assertIsNotNone(event_formatter)
+
+  def testGetFormatStringAttributeNames(self):
+    """Tests the GetFormatStringAttributeNames function."""
+    event_formatter = the_plugin.ThePluginNumerictypesFormatter()
+
+    expected_attribute_names = [
+        u'booleanval',
+        u'datetimeval',
+        u'dateval',
+        u'decimalval',
+        u'numericval'
+        ]
+
+    self._TestGetFormatStringAttributeNames(
+        event_formatter, expected_attribute_names)
+
+class ThePluginRealtypesFormatterTest(test_lib.EventFormatterTestCase):
+  """Tests the The Plugin realtypes event formatter."""
+
+  def testInitialization(self):
+    """Tests the initialization."""
+    event_formatter = the_plugin.ThePluginRealtypesFormatter()
+    self.assertIsNotNone(event_formatter)
+
+  def testGetFormatStringAttributeNames(self):
+    """Tests the GetFormatStringAttributeNames function."""
+    event_formatter = the_plugin.ThePluginRealtypesFormatter()
+
+    expected_attribute_names = [
+        u'doubleprecesionval',
+        u'doubleval',
+        u'floatval',
+        u'realval'
+        ]
+
+    self._TestGetFormatStringAttributeNames(
+        event_formatter, expected_attribute_names)
+
+class ThePluginTexttypesFormatterTest(test_lib.EventFormatterTestCase):
+  """Tests the The Plugin texttypes event formatter."""
+
+  def testInitialization(self):
+    """Tests the initialization."""
+    event_formatter = the_plugin.ThePluginTexttypesFormatter()
+    self.assertIsNotNone(event_formatter)
+
+  def testGetFormatStringAttributeNames(self):
+    """Tests the GetFormatStringAttributeNames function."""
+    event_formatter = the_plugin.ThePluginTexttypesFormatter()
+
+    expected_attribute_names = [
+        u'characterval',
+        u'clobval',
+        u'nativecharacterval',
+        u'ncharval',
+        u'nvarchar_val',
+        u'textval',
+        u'varcharval',
+        u'varyingcharacterval'
+        ]
+
+    self._TestGetFormatStringAttributeNames(
+        event_formatter, expected_attribute_names)
+
+class ThePluginNodataFormatterTest(test_lib.EventFormatterTestCase):
+  """Tests the The Plugin nodata event formatter."""
+
+  def testInitialization(self):
+    """Tests the initialization."""
+    event_formatter = the_plugin.ThePluginNodataFormatter()
+    self.assertIsNotNone(event_formatter)
+
+  def testGetFormatStringAttributeNames(self):
+    """Tests the GetFormatStringAttributeNames function."""
+    event_formatter = the_plugin.ThePluginNodataFormatter()
+
+    expected_attribute_names = [
+        u'bigintval',
+        u'blobval',
+        u'booleanval',
+        u'characterval',
+        u'clobval',
+        u'datetimeval',
+        u'dateval',
+        u'decimalval',
+        u'doubleprecisionval',
+        u'doubleval',
+        u'floatval',
+        u'int2val',
+        u'int8val',
+        u'integerval',
+        u'intval',
+        u'mediuintval',
+        u'nativecharacterval',
+        u'ncharval',
+        u'numericval',
+        u'nvarcharval',
+        u'realval',
+        u'smallintval',
+        u'textval',
+        u'tinyintval',
+        u'unsignedbigintval',
+        u'varcharval',
+        u'varyingcharacterval'
         ]
 
     self._TestGetFormatStringAttributeNames(
