92e1828855e34f202c2860c2d7504fe3a5765916
Claudia Saxer <c.s.saxer@gmail.com>
2017-05-08 08:16:06 +0200

    datamodel update

--- src/plasoscaffolder/model/sql_query_column_model_data.py
+++ src/plasoscaffolder/model/sql_query_column_model_data.py
@@ -1,28 +1,28 @@
 # -*- coding: utf-8 -*-
 """Model for SQL column."""
+from plasoscaffolder.model import sql_query_column_model_detailed
 
-from plasoscaffolder.model import sql_query_column_model
 
-
-class SQLColumnModelData(sql_query_column_model.SQLColumnModel):
+class SQLColumnModelData(object):
   """Class for columns of a SQL Query."""
 
-  def __init__(self, sql_column: str, sql_column_type: type = None, data=None,
-               expected_message=None):
-    """ initializes the SQL column model.
+  def __init__(
+      self, expected_message: {str: str},
+      columns_detailed: [sql_query_column_model_detailed.SQLColumnModelDetailed]
+  ):
+    """initializes the SQL column model.
   
     Args:
-      sql_column (str): the column name of the SQL Query
-      sql_column_type (str): the type of the SQL column
-      data (str, str): the data for the timestamp (key, value)
-      expected_message (str, str): the expected Messages for the timestamps (
-          key, value)
+      expected_message ({key:value}): the expected Messages for the 
+          timestamps {key:value}
+      columns_detailed ([
+          sql_query_column_model_detailed.SQLColumnModelDetailed]): the columns
+          with detailed information for the data columns
     """
-    super().__init__(sql_column, sql_column_type)
+    super().__init__()
     self._expected_message = expected_message
-    self._data = data
+    self._columns_detailed = columns_detailed
 
-"""todo : move messages in new class as top and data as item"""
   @property
   def ExpectedMessages(self) -> (str, str):
     """The Expected Message for the timestamp
@@ -41,24 +41,11 @@ class SQLColumnModelData(sql_query_column_model.SQLColumnModel):
     """
     return self._expected_message[0:77]
 
-  def FirstEventPositionInEvents(self) -> int:
-    """
-    
-    Args:
-      self: 
+  @property
+  def Columns(self) -> ([sql_query_column_model_detailed.SQLColumnModelDetailed]):
+    """The columns with detailed information for the data columns
 
     Returns:
+      ([sql_query_column_model_detailed.SQLColumnModelDetailed]): the columns
     """
-
-
-"""move until here"""
-  def FirstDataForTimeEvent(self, timestamp: str) -> str:
-    """The Data for the Time Event
-    
-    Args:
-      timestamp: the timestamp column name
-  
-    Returns:
-      str: the data for the timestamp
-    """
-    return self._data.get(timestamp, 'TODO')
+    return self._columns_detailed
