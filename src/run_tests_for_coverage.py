99ee632ee6cf8fb3698c1ea19ca6a6ae236faab7
Claudia Saxer <c.s.saxer@gmail.com>
2017-05-09 11:12:30 +0200

    seperate integrationtests and unittests for coverage

---
+++ src/run_tests_for_coverage.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""runs all tests"""
+import sys
+import unittest
+
+if __name__ == '__main__':
+  test_suite = unittest.TestLoader().discover('tests/unittests', pattern='*.py')
+
+  test_results = unittest.TextTestRunner(verbosity=2).run(test_suite)
+  if not test_results.wasSuccessful():
+    sys.exit(1)
---
+++ src/tests/integrationtests/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+# those tests will no be run in coverage
