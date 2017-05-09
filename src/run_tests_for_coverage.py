4533863395854df841f5727803b9bd7cde9e5d59
Claudia Saxer <c.s.saxer@gmail.com>
2017-05-09 11:18:51 +0200

    use different seperator

--- src/run_tests_for_coverage.py
+++ src/run_tests_for_coverage.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 """runs all tests"""
+import os
 import sys
 import unittest
 
 if __name__ == '__main__':
-  test_suite = unittest.TestLoader().discover('tests/unittests', pattern='*.py')
-
+  unittest_path = '{0}{1}{2}'.format('tests',os.sep,'unittests')
+  test_suite = unittest.TestLoader().discover(unittest_path, pattern='*.py')
   test_results = unittest.TextTestRunner(verbosity=2).run(test_suite)
   if not test_results.wasSuccessful():
     sys.exit(1)
