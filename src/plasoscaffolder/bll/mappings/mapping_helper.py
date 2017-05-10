0e940e65cae35f12328ee9094eb07ef334a53676
Claudia Saxer <c.s.saxer@gmail.com>
2017-05-10 11:13:40 +0200

    cleanup and test helper more

--- src/plasoscaffolder/bll/mappings/mapping_helper.py
+++ src/plasoscaffolder/bll/mappings/mapping_helper.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """Helper methods for mapping."""
+import re
+
 import jinja2
 
 from plasoscaffolder.bll.mappings import base_mapping_helper
@@ -20,7 +22,7 @@ class MappingHelper(base_mapping_helper.BaseMappingHelper):
     template_loader = jinja2.FileSystemLoader(template_path)
     self._template_environment = jinja2.Environment(
         autoescape=False, loader=template_loader, trim_blocks=False)
-    self.yapf_path = yapf_path
+    self.formatter = code_formatter.CodeFormatter(yapf_path)
 
   def RenderTemplate(self, template_filename: str, context: dict) -> str:
     """Renders the template.
@@ -32,22 +34,16 @@ class MappingHelper(base_mapping_helper.BaseMappingHelper):
        Returns:
          str: the rendered template
        """
+
     template = self._template_environment.get_template(
         template_filename).render(context)
-    # Because jinja template variable is first escaped and then word wrapped,
-    # the escaped backslash can be split and can result in an eol.
-    # The escaped backslash will be placed on the next line.
-    template = template.replace('\\\'\n        u\'\\','\'\n        u\'\\\\')
+    template = self._RemoveEscapeError(template)
 
-    formatter = code_formatter.CodeFormatter(self.yapf_path)
+    formatted = self.formatter.Format(template)[0]
+    formatted = self._RemoveYapfComment(formatted)
+    formatted = self._RemoveBlanksAtEndOfLine(formatted)
 
-    formatted = formatter.Format(template)
-    # Removing the yapf comment because it should not be in the resulting file.
-    formatted_removed_yapf = formatted[0].replace(
-        '# yapf: disable\n', '').replace('# yapf: enable\n', '').replace('  \n',
-                                                                         '\n')
-    return formatted_removed_yapf
-    """return template"""
+    return formatted
 
   def GenerateClassName(self, plugin_name: str) -> str:
     """Generates the class name from the plugin name.
@@ -59,3 +55,56 @@ class MappingHelper(base_mapping_helper.BaseMappingHelper):
       str: the class name
     """
     return plugin_name.replace('_', ' ').title().replace(' ', '')
+
+  def _RemoveEscapeError(self, template: str) -> str:
+    """Remove the escape error.
+    
+    Because jinja template variable is first escaped and then word wrapped, 
+    the escaped backslash can be split and can result in an eol.
+    The escaped backslash will be placed on the next line.
+    This is a workaround and can be removed if yapf supports unicode string
+    formatting and it is also changed in the jinja2 template, and only works
+    with 8 spaces.
+    
+    Args:
+      template (str): the resulting template as a python file string
+
+    Returns:
+      str: the template without escape (eol) errors
+    """
+
+    to_be_replaced = '\\\'\n        u\'\\'
+    to_be_replaced_with = '\'\n        u\'\\\\'
+
+    template = template.replace(to_be_replaced,to_be_replaced_with)
+    return template
+
+  def _RemoveYapfComment(self, template: str) -> str:
+    """Remove the yapf comment line
+    
+    The Line as well as the new line will be removed.
+    The Yapf Comment has to be at the end of the line. Or on its own line.
+    
+    Args:
+      template (str): the resulting template as a python file string 
+
+    Returns:
+      str: the template without yapf comment lines
+    """
+    return template.replace('# yapf: disable\n', '').replace('# yapf: enable\n',
+                                                             '')
+
+  def _RemoveBlanksAtEndOfLine(self, template: str) -> str:
+    """Removes blanks at the end of lines
+    
+    This is for those parts that are ignored with yapf.
+    
+    Args:
+      template (str): the template to remove the blanks at the end of lines
+
+    Returns:
+      str: the template without blanks on the line endings
+    """
+    while template.find('  \n') != -1:
+      template = template.replace('  \n', '\n')
+    return template
