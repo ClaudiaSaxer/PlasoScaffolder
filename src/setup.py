8840619dbf2d7e87a77f3df5623049faa9c0d25c
Claudia Saxer <c.s.saxer@gmail.com>
2017-05-22 10:05:36 +0200

    set version in setup

--- src/setup.py
+++ src/setup.py
@@ -9,8 +9,12 @@ setup(name='plasoscaffolder',
       packages=find_packages(),
       include_package_data=True,
       package_data={'plasoscaffolder.bll.templates': ['*.jinja2'],'':['.style.yapf']},
-      install_requires=['Click', 'setuptools', 'jinja2', 'colorama',
-                        'yapf', 'pexpect'],
+      install_requires=['Click>=6.7',
+                        'setuptools>=35.0.2',
+                        'jinja2>=2.9.6',
+                        'colorama>=0.3.7',
+                        'yapf==0.16.2',
+                        'pexpect>=4.2.1'],
       entry_points={'console_scripts': [
           'plasoscaffolder=plasoscaffolder.frontend.main:entry_point']}
      )
